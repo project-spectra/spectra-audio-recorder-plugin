f6380e875313140e9ed7e37358fd99f8f2119897
ozzmotic <ozzmotic@gmail.com>
2019-09-23 09:35:25 -0400

    Fill in android.ts

--- src/spectra-audio-recorder-plugin.android.ts
+++ src/spectra-audio-recorder-plugin.android.ts
@@ -3,7 +3,22 @@ import { Common } from './spectra-audio-recorder-plugin.common';
 declare var org: any;
 
 export class SpectraAudioRecorderPlugin {
+
+    private _audioRecorder: any;
+
     public HelloWorld(): string {
         return org.project_spectra.spectraaudiorecorder.AudioRecorder.HelloWorld();
+    }
+
+    public launchTask(recordingPath: String): void {
+        this._audioRecorder = new org.project_spectra.spectraaudiorecorder.AudioRecorder();
+
+        this._audioRecorder.launchTask(recordingPath);
+        //?org.project_spectra.spectraaudiorecorder.AudioRecorder.launchTask(recordingPath);
+    }
+
+    public stopTask(): void {
+        this._audioRecorder.stopTask();
+        //?org.project_spectra.spectraaudiorecorder.AudioRecorder.stopTask();
     }
 }
