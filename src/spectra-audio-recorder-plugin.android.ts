a8a6c5e329ecb483ff59810001c754348ce93aea
Bryan Kok <bryan.wyern1@gmail.com>
2019-10-01 12:13:35 +0800

    Write WAV file to external storage

--- src/spectra-audio-recorder-plugin.android.ts
+++ src/spectra-audio-recorder-plugin.android.ts
@@ -4,9 +4,11 @@ declare var org: any;
 
 export class SpectraAudioRecorderPlugin {
 
-    private _audioRecorderAsyncTask: any;
+    private _audioRecorder: any;
 
     public HelloWorld(): string {
+        console.log(org.project_spectra.spectraaudiorecorder.AudioRecorder.HelloWorld());
+        console.log(org.project_spectra.spectraaudiorecorder.AudioRecorder.WorldHello());
         return org.project_spectra.spectraaudiorecorder.AudioRecorder.HelloWorld();
     }
 
@@ -14,16 +16,27 @@ export class SpectraAudioRecorderPlugin {
         console.log('Audio Recorder Plugin Starting.');
         console.log(recordingPath);
 
+        this._audioRecorder = new org.project_spectra.spectraaudiorecorder.AudioRecorder();
+        console.log(this._audioRecorder);
+
+        console.log(org.project_spectra.spectraaudiorecorder.AudioRecorder.HelloWorld());
+        console.log(org.project_spectra.spectraaudiorecorder.AudioRecorder.WorldHello());
+
         return new Promise (async (resolve, reject) => {
             try {
-                this._audioRecorderAsyncTask =
-                new org.project_spectra.spectraaudiorecorder.AudioRecorder.RecordWaveTask();
+                this._audioRecorder = new org.project_spectra.spectraaudiorecorder.AudioRecorder();
+                console.log(this._audioRecorder);
 
-                //argument should be the File file.wav
-                //var wavFile = new File([""], recordingPath);
+                // argument should be the File file.wav
+                // var wavFile = new File([""], recordingPath);
                 console.log("Recording to: " + recordingPath);
-                
-                this._audioRecorderAsyncTask.execute(recordingPath);
+                this._audioRecorder.startTask(recordingPath, new org.project_spectra.spectraaudiorecorder.OnFileWrittenListener({
+                    onFileWrittenCallback: (fileSize, elapsedMs) => {
+                        console.log(fileSize, elapsedMs);
+                        resolve({fileSize, elapsedMs});
+                    }
+                }));
+
             } catch (ex) {
                 console.log(ex);
                 reject(ex);
@@ -31,11 +44,14 @@ export class SpectraAudioRecorderPlugin {
         });
     }
 
+    /**
+     * Stops the task - the actual writing of the WAV to disk may take some time, thus accepts a callback.
+     */
     public stopTask() {
         return new Promise (async (resolve, reject) => {
             try {
-                this._audioRecorderAsyncTask.cancel(true);
-                this._audioRecorderAsyncTask = null;
+                this._audioRecorder.stopTask();
+                // this._audioRecorderAsyncTask = null;
             } catch (ex) {
                 console.log(ex);
                 reject(ex);
